814c0a7907fc93a483669251d5d89381fdd15a49
jseam2 <hello.jseam@gmail.com>
2024-11-17 16:28:53 +0700

    init horntdao

---
+++ src/components/Web3Provider.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { base } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+
+const config = createConfig(
+  getDefaultConfig({
+    // Your dApps chains
+    chains: [base],
+    transports: {
+      // RPC URL for each chain
+      [base.id]: http('https://rpc.ankr.com/base')
+    },
+
+    // Required API Keys
+    walletConnectProjectId: "d994879049144752568a2ff69862155a",
+
+    // Required App Info
+    appName: "HorntDAO",
+
+    // Optional App Info
+    appDescription: "Public Goods For Horny People",
+    appUrl: "https://family.co", // your app's url
+    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  }),
+);
+
+const queryClient = new QueryClient();
+
+export const Web3Provider = ({ children }: { children: any }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider>{children}</ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
