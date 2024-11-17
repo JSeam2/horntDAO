9939e1d8e62e826a8c00aead03786368bc50fb28
jseam2 <hello.jseam@gmail.com>
2024-11-17 18:19:24 +0700

    fix: change name to hornydao

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,7 +9,7 @@ export default function NavBar(): JSX.Element {
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
                 {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo"> */}
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">HorntDAO</span>
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">HornyDAO</span>
             </a>
             <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
               <ConnectKitButton showAvatar  />
